7eb1e946fdda10fe132e8975f6b33c8249f242d0
ahiadl <ahiadlevi1989@gmail.com>
2017-01-18 19:49:48 +0200

    Winter is Comming

--- GPS-INS-NAV/Debug/common/startup_ccs.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-common/startup_ccs.obj: ../common/startup_ccs.c
-
-../common/startup_ccs.c: 
--- GPS-INS-NAV/Debug/raw_input_to_data.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-raw_input_to_data.obj: ../raw_input_to_data.c
-
-../raw_input_to_data.c: 
